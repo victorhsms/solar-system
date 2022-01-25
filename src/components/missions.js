3e953e942a3d42bbaff0f0bd15bc670c7f20e1db
victorhsmsantos <victorhsmsantos@gmail.com>
2022-01-25 16:38:49 -0300

    Trocado o uso de index por nanoid

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 import Title from './title';
 import MissionCard from './missionCard';
 import missions from '../data/missions';
+
+// Referencia do 'nanoid' comentada nas linhas 7..9 do arquivo ./solarSystem.js
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map((mission, index) => {
+        {missions.map((mission) => {
           const { name, year, country, destination } = mission;
           return (<MissionCard
-            key={ index }
+            key={ nanoid() }
             name={ name }
             year={ year }
             country={ country }
--- src/components/solarSystem.js
+++ src/components/solarSystem.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 import Title from './title';
 import PlanetCard from './planetCard';
 import planets from '../data/planets';
+
+// Referencia nanoid: https://github.com/ai/nanoid/
+// Ideia de uso do nanoid do colega da tribo B(turma XP) Asafe Dainez em um post do slack[https://trybecourse.slack.com/archives/C02HY11SPJP/p1643138374441800]
+// Nano ID é um gerador de IDs de 21 caracteres/símbolos que ja vem no NODE e, segundo a sua documentação, ele é performático e seguro.
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {planets.map((planet, index) => {
+        {planets.map((planet) => {
           const { name, image } = planet;
           return (<PlanetCard
-            key={ index }
+            key={ nanoid() }
             planetName={ name }
             planetImage={ image }
           />);
