d1ff867ccf3d23e1cbc84ec88a75cd7d8963fc57
victorhsmsantos <victorhsmsantos@gmail.com>
2022-01-25 14:58:10 -0300

    Segundo requisito feito!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/header';
+import SolarSystem from './components/solarSystem';
 
 class App extends React.Component {
   render() {
-    return (<Header />);
+    return (
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
+    );
   }
 }
 
